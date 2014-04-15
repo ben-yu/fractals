f03414b15e061ed79d4a45a5847f581f4b94f7cd
Benjamin Yu <benjamin.sichit.yu@gmail.com>
2014-04-15 19:15:58 -0400

    color change

--- js/templates.js
+++ js/templates.js
@@ -1,6 +1,1 @@
-window.require.register("index", function(exports, require, module) {
-  module.exports = function anonymous(locals) {
-  var buf = [];
-  buf.push("<div id=\"shaderFractal\"></div><script src=\"/js/vendor.js\"></script><script src=\"/js/app.js\"></script><script>require('scripts/fractal')</script>");;return buf.join("");
-  };
-});
+window.require.register("index",function(exports,require,module){module.exports=function anonymous(locals){var buf=[];buf.push("<div id=\"shaderFractal\"></div><script src=\"./js/vendor.js\"></script><script src=\"./js/app.js\"></script><script>require('scripts/fractal')</script>");return buf.join("")}});
